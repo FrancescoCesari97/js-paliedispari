3c9341199f4a41ce89dfea139d8435b22e5a0cb9
FrancescoCesari97 <freedus97@gmail.com>
2024-01-17 17:06:04 +0100

    completato esercizio palindromo

--- functions.js
+++ functions.js
@@ -5,12 +5,9 @@
 //- verificare se le parole sono ugali se si stamapre palindroma se no stampare non palindroma
 
 function askWord() {
-  //- chiedere con un prompt di inserire una parola
-  let userWord = prompt("scrivi una parola");
-
   //- invertire la parola con un ciclo for
   let reverseWord = "";
-  for (let i = userWord.length; i >= 0; i--) {
+  for (let i = userWord.length - 1; i >= 0; i--) {
     reverseWord += userWord[i];
   }
 
--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-askWord();
+const resultElement = document.getElementById("result");
+
+//- chiedere con un prompt di inserire una parola
+let userWord = prompt("scrivi una parola");
+
+//- verificare se le parole sono ugali se si stamapre palindroma se no stampare non palindroma
+
+let reverseWord = askWord();
+if (userWord == reverseWord) {
+  resultElement.innerHTML = "palindroma";
+} else if (userWord != reverseWord) {
+  resultElement.innerHTML = "non palindroma";
+}
