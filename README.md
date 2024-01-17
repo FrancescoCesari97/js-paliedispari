f7746c4d5a688c25f79dcc02558574a0ad068370
FrancescoCesari97 <freedus97@gmail.com>
2024-01-17 16:39:39 +0100

    aggiunta funzioni per invertire la parola

---
+++ README.md
@@ -0,0 +1,11 @@
+## Palidroma
+
+- Chiedere all'utente di inserire una parola
+- Creare una funzione per capire se la parola inserita è palindroma
+
+## svolgimento
+
+- chiedere con un prompt di inserire una parola
+- invertire la parola con un ciclo for
+  - salvare la parola invertita in un altra variabile
+- verificare se le parole sono ugali se si stamapre palindroma se no stampare non palindroma
---
+++ functions.js
@@ -0,0 +1,21 @@
+//- chiedere con un prompt di inserire una parola
+//- invertire la parola con un ciclo for
+//    - salvare la parola invertita in un altra variabile
+//- ritorna la parola invertita
+//- verificare se le parole sono ugali se si stamapre palindroma se no stampare non palindroma
+
+function askWord() {
+  //- chiedere con un prompt di inserire una parola
+  let userWord = prompt("scrivi una parola");
+
+  //- invertire la parola con un ciclo for
+  let reverseWord = "";
+  for (let i = userWord.length; i >= 0; i--) {
+    reverseWord += userWord[i];
+  }
+
+  console.log(reverseWord);
+
+  //- ritorna la parola invertita
+  return reverseWord;
+}
